deecac98061a8a905316fc99e95543294d29d448
Stefun22 <stefunraj1999@gmail.com>
2024-07-28 00:23:02 +0530

    UI Changes for mobile app screen

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -7,12 +7,11 @@ class Footer extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(20.0),
       color: Colors.green.withOpacity(0.5),
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: LayoutBuilder(builder: (ctx, c) {
+        if (c.maxWidth < 900) {
+          return Column(
             children: [
+              const SizedBox(height: 10),
               const Text(
                 'Follow me on social media :',
                 style: TextStyle(
@@ -20,29 +19,70 @@ class Footer extends StatelessWidget {
                     color: Colors.black,
                     fontWeight: FontWeight.bold),
               ),
-              IconButton(
-                  icon: networkImage(
-                      "https://cdn-icons-png.flaticon.com/128/1384/1384014.png"),
-                  onPressed: () async =>
-                      await "https://www.linkedin.com/in/stefun-s-2a6975228"
-                          .toUri()
-                          .launch()),
-              IconButton(
-                  icon: networkImage(
-                      "https://cdn-icons-png.flaticon.com/128/1384/1384007.png"),
-                  onPressed: () async =>
-                      await "https://wa.me/919790164675".toUri().launch()),
-              IconButton(
-                  icon: networkImage(
-                      "https://cdn-icons-png.flaticon.com/128/1384/1384015.png"),
-                  onPressed: () async =>
-                      "https://www.instagram.com/s_t_e_f_u_n/"
-                          .toUri()
-                          .launch()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                      icon: networkImage(
+                          "https://cdn-icons-png.flaticon.com/128/1384/1384014.png"),
+                      onPressed: () async =>
+                          await "https://www.linkedin.com/in/stefun-s-2a6975228"
+                              .toUri()
+                              .launch()),
+                  IconButton(
+                      icon: networkImage(
+                          "https://cdn-icons-png.flaticon.com/128/1384/1384007.png"),
+                      onPressed: () async =>
+                          await "https://wa.me/919790164675".toUri().launch()),
+                  IconButton(
+                      icon: networkImage(
+                          "https://cdn-icons-png.flaticon.com/128/1384/1384015.png"),
+                      onPressed: () async =>
+                          "https://www.instagram.com/s_t_e_f_u_n/"
+                              .toUri()
+                              .launch()),
+                ],
+              ),
             ],
-          ),
-        ],
-      ),
+          );
+        }
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Follow me on social media :',
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                    icon: networkImage(
+                        "https://cdn-icons-png.flaticon.com/128/1384/1384014.png"),
+                    onPressed: () async =>
+                        await "https://www.linkedin.com/in/stefun-s-2a6975228"
+                            .toUri()
+                            .launch()),
+                IconButton(
+                    icon: networkImage(
+                        "https://cdn-icons-png.flaticon.com/128/1384/1384007.png"),
+                    onPressed: () async =>
+                        await "https://wa.me/919790164675".toUri().launch()),
+                IconButton(
+                    icon: networkImage(
+                        "https://cdn-icons-png.flaticon.com/128/1384/1384015.png"),
+                    onPressed: () async =>
+                        "https://www.instagram.com/s_t_e_f_u_n/"
+                            .toUri()
+                            .launch()),
+              ],
+            ),
+          ],
+        );
+      }),
     );
   }
 
--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -36,12 +36,64 @@ class ProjectCard extends StatelessWidget {
           return LayoutBuilder(builder: (context, c) {
             if (c.maxWidth < 900) {
               return Card(
+                  color: Colors.white.withOpacity(0.1),
                   margin: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                          width: 100, child: Image.network(project.company)),
-                    ],
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 20),
+                        Text(
+                          project.title,
+                          style: const TextStyle(fontSize: 22),
+                        ),
+                        const SizedBox(height: 20),
+                        FutureBuilder<String?>(
+                            future: ProjectViewModel.downloadURLExample(
+                                fileName: "${project.title}.jpeg"),
+                            builder: (context, ds) {
+                              return Container(
+                                width: Sizes.height / 3.5,
+                                height: Sizes.height / 3.5,
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(25)),
+                                    image: ds.connectionState ==
+                                            ConnectionState.waiting
+                                        ? null
+                                        : DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: NetworkImage(ds.data!),
+                                            scale: 1)),
+                                child: ds.connectionState ==
+                                        ConnectionState.waiting
+                                    ? const CircularProgressIndicator()
+                                    : const SizedBox(),
+                              );
+                            }),
+                        const SizedBox(height: 30),
+                        Column(
+                            children: project.description
+                                .map(
+                                  (e) => SizedBox(
+                                    width: Sizes.width / 1,
+                                    child: ListTile(
+                                      leading: const Icon(
+                                        Icons.arrow_circle_right_outlined,
+                                        color: Colors.lightGreen,
+                                      ),
+                                      title: Text(
+                                        e,
+                                        style: const TextStyle(
+                                            fontSize: 20, color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                                .toList())
+                      ],
+                    ),
                   ));
             }
             return InkWell(
